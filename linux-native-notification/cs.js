fc1082269cc03d0aa034e7de64d79caca2e12986
Tualatrix Chou <tualatrix@gmail.com>
2012-05-06 18:25:08 +0800

    Implement the linux-native-notification

---
+++ linux-native-notification/cs.js
@@ -0,0 +1,30 @@
+// STEP 1: Inject page.js into the actual page's DOM so we can interact with said page
+
+var s = document.createElement("script");
+s.src = chrome.extension.getURL("page.js");
+s.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(s);
+
+// STEP 2: ????
+
+document.addEventListener("chromifyOSDComEvent", function(e)
+{
+    var event_data = document.getElementById("chromifyOSDComObject");
+    var title = event_data.firstChild.innerHTML;
+    var body = event_data.firstChild.nextSibling.innerHTML;
+    var iconUrl = event_data.firstChild.nextSibling.nextSibling.innerHTML;
+    var notificationType = event_data.firstChild.nextSibling.nextSibling.nextSibling.innerHTML;
+    event_data.parentNode.removeChild(event_data);
+
+    chrome.extension.sendRequest(
+    {
+        title: title, body: body, iconUrl: iconUrl, notificationType: notificationType
+    },
+    function(response)
+    {
+        console.log(response);
+        // do something, or nothing, depending on the response from background.html
+    });
+});
+
+// STEP 3: PROFIT!!
